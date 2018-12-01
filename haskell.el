c56b7b8623e0a0c5bec641184df217c10460f4a0
Michael Sloan <mgsloan@gmail.com>
2018-12-01 05:45:18 -0800

    Misc updates to haskell config

--- haskell.el
+++ haskell.el
@@ -1,3 +1,16 @@
 (use-package haskell-mode
-  :init
-  (add-hook 'haskell-mode-hook (lambda () (setq-local eldoc-documentation-function nil))))
+  :mode (("\\.hs\\(c\\|-boot\\)?\\'" . haskell-mode)
+         ("\\.lhs\\'" . literate-haskell-mode)
+         ("\\.cabal\\'" . haskell-cabal-mode))
+  :bind (:map haskell-mode-map
+              ("C-c ," . haskell-navigate-imports)
+              ("C-c C-." . haskell-mode-format-imports)
+              ("C-c C-u" . my-haskell-insert-undefined))
+  :preface
+  (defun my-haskell-insert-undefined ()
+    (interactive) (insert "undefined"))
+  )
+
+;; TODO:
+;;
+;; * interactive version of haskell-string-literal-encode and decode
