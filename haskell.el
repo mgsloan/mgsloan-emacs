f8e52bca06de3e5323363d099fbc6388a68e5876
Michael Sloan <mgsloan@gmail.com>
2018-12-01 17:59:25 -0800

    Add bindings for moving nested indentation left / right

--- haskell.el
+++ haskell.el
@@ -5,7 +5,10 @@
   :bind (:map haskell-mode-map
               ("C-c ," . haskell-navigate-imports)
               ("C-c C-." . haskell-mode-format-imports)
-              ("C-c C-u" . my-haskell-insert-undefined))
+              ("C-c C-u" . my-haskell-insert-undefined)
+              ("M-<left>" . haskell-move-nested-left)
+              ("M-<right>" . haskell-move-nested-right)
+        )
   :preface
   (defun my-haskell-insert-undefined ()
     (interactive) (insert "undefined"))
