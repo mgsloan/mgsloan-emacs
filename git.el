7e21ccc2700c3a66d161046b085c9bbc3608c6d7
Michael Sloan <mgsloan@gmail.com>
2020-03-28 01:18:24 -0600

    Add ~/Arduino/Model01-Firmware to repo list

--- git.el
+++ git.el
@@ -57,6 +57,7 @@
               ("~/proj/th-utilities" . DEPTH0)
               ("~/proj/todoist-shortcuts" . DEPTH0)
               ("~/proj/unblock-with-intention" . DEPTH0)
+              ("~/Arduino/Model01-Firmware" . DEPTH0)
               ("~/docs" . DEPTH0)
               ("~/.emacs.d" . DEPTH0))))
   (evil-define-key 'motion magit-repolist-mode-map (kbd "g")
