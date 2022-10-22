a7b3e825b3c63498fee3cf5db6d2f14fd26b4d53
Michael Sloan <mgsloan@gmail.com>
2022-10-21 22:43:42 -0600

    Bring back haskell-mode

--- haskell.el
+++ haskell.el
@@ -1,21 +1,9 @@
 ;; Sadly not writing much haskel lthese days, not really using haskell-mode.
 
-;; (use-package haskell-mode
-;;   :mode (("\\.hs\\(c\\|-boot\\)?\\'" . haskell-mode)
-;;          ("\\.lhs\\'" . literate-haskell-mode)
-;;          ("\\.cabal\\'" . haskell-cabal-mode))
-;;   :bind (:map haskell-mode-map
-;;               ("C-c ," . haskell-navigate-imports)
-;;               ("C-c C-." . haskell-mode-format-imports)
-;;               ("C-c C-u" . my-haskell-insert-undefined)
-;;               ("M-<left>" . haskell-move-nested-left)
-;;               ("M-<right>" . haskell-move-nested-right)
-;;         )
-;;   :preface
-;;   (defun my-haskell-insert-undefined ()
-;;     (interactive) (insert "undefined"))
-;;   )
-
+(use-package haskell-mode
+  :mode (("\\.hs\\(c\\|-boot\\)?\\'" . haskell-mode)
+         ("\\.lhs\\'" . literate-haskell-mode)
+         ("\\.cabal\\'" . haskell-cabal-mode)))
 
 
 ;; Old commented out stuff
