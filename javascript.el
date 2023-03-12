3ffeb8df27d1a4b7efb118c532289e024e0cf47b
Michael Sloan <mgsloan@gmail.com>
2023-03-11 19:07:31 -0700

    Use js2-mode

--- javascript.el
+++ javascript.el
@@ -1,4 +1,9 @@
+(use-package js2-mode
+  :defer t
+  :mode (("\\.js$" . js2-mode))
+  :config
+  (setq js2-basic-offset 2)
+  (setq js2-indent-switch-body t))
+
 (setq js-indent-level 2)
 (setq js-indent-offset 2)
-(setq js2-basic-offset 2 js-indent-level 2)
-(setq js2-indent-switch-body t)
